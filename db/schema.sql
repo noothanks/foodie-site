0a65e44dcd138019dd4f729d89441f3900966343
susangrace909 <102876452+susangrace909@users.noreply.github.com>
2022-07-22 12:49:32 -0400

    added users table and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,34 @@
-DROP DATABASE IF EXISTS foodie_world
+DROP TABLE IF EXISTS nationalities;
+DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS users;
 
-CREATE DATABASE foodie_world;
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(20) NOT NULL,
+  last_name VARCHAR(20) NOT NULL,
+  email VARCHAR(40) NOT NULL,
+  pword VARCHAR(15) NOT NULL,
+  username VARCHAR(20) NOT NULL,
+  birthday DATE,
+  /* food preferences checklist? */
+);
+
+/* just titled, didn't add info*/
+CREATE TABLE recipes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+/* just titled, didn't add info */ 
+CREATE TABLE nationalities (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
