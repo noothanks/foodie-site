7ddbca1283ac29fb8c6e99f6f7c93eabb18ad515
noothanks <coreyfuller727@yahoo.com>
2022-07-31 18:23:28 -0400

    testing port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require("express-handlebars");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
